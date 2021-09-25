50cfd826341ee3a07716575935f204b443838bb7
Nzitatira Hirwa Kevin <Nzitatirahkevin@gmail.com>
2021-09-25 12:22:46 +0200

    Edit apps in settings.py

--- Quotes/settings.py
+++ Quotes/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.app.CoreConfig',
+    'core.apps.CoreConfig',
     'corsheaders',
     'rest_framework',
     'django.contrib.admin',
