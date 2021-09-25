b5a7031f9bd55d6b6b10992c437cde908daeba6c
Nzitatira Hirwa Kevin <Nzitatirahkevin@gmail.com>
2021-09-25 11:50:39 +0200

    Remove security data

--- Quotes/settings.py
+++ Quotes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz7a-$ot^pvebspvjk=8cjzmv84w_*3hw^jt*5jz-hkf@w2k3)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
